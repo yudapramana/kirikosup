eb18b9327ab1b4ff7a45da5b398f2ccde6db18be
pramana yuda <pramanayuda772@gmail.com>
2024-02-15 09:11:25 +0700

    resolve alias of vue

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -20,7 +20,7 @@
 
     {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
 
-    @vite('resources/js/app.js')
+    {{-- @vite('resources/js/app.js') --}}
 
     <!-- add this code -->
     {{-- <link rel="stylesheet" href="{{ $css }}" id="css">
