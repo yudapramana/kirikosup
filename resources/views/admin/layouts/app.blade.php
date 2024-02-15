4d532786639054dd1c5ab0af2350783c4b7171c0
pramana yuda <pramanayuda772@gmail.com>
2024-02-15 09:18:37 +0700

    resolve alias of vue

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -18,7 +18,7 @@
     @endphp --}}
 
 
-    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     {{-- @vite('resources/js/app.js') --}}
 
