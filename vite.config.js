d2f8b20e0fd6c057c2b97075adf1be6db6c50a02
pramana yuda <pramanayuda772@gmail.com>
2024-02-15 18:30:33 +0700

    checkpoint on printing and other things

--- vite.config.js
+++ vite.config.js
@@ -25,16 +25,5 @@ export default defineConfig({
             //   vue: "vue/dist/vue.esm-bundler.js",
         },
     },
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                    }
-                }
-            }
-        }
-    }
     //   envPrefix: ["VITE_", "MIX_"],
 });
