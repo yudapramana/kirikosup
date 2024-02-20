624569c6f6441c75c8762f631edc68fec5eaa0c6
pramana yuda <pramanayuda772@gmail.com>
2024-02-20 20:06:08 +0700

    push all

--- resources/js/components/SidebarLeft.vue
+++ resources/js/components/SidebarLeft.vue
@@ -27,8 +27,8 @@ const logout = () => {
 <template>
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
 
-        <a href="index3.html" class="brand-link">
-            <img src="https://adminlte.io/themes/v3/dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
+        <a href="#" class="brand-link">
+            <img src="/app_logo.png" alt="AdminLTE Logo"
                 class="brand-image img-circle elevation-3" style="opacity: .8">
             <span class="brand-text font-weight-light">{{ settingStore.setting.app_name }}</span>
         </a>
