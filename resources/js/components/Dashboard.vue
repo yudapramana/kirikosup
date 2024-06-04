254480e62fc1a7747242c3f98014ef3d151142a8
pramana yuda <pramanayuda772@gmail.com>
2024-06-04 16:23:19 +0700

    push new dashboard

--- resources/js/components/Dashboard.vue
+++ resources/js/components/Dashboard.vue
@@ -38,7 +38,7 @@ onMounted(() => {
                 <div class="col-sm-6">
                     <h1 class="m-0">Beranda</h1>
                 </div>
-                <div class="col-sm-6"  v-if="!screenDisplayStore.isMobile">
+                <div class="col-sm-6" v-if="!screenDisplayStore.isMobile">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="#">Home</a></li>
                         <li class="breadcrumb-item active">Beranda</li>
@@ -52,7 +52,45 @@ onMounted(() => {
     <div class="content">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-lg-3 col-12 col-md-4">
+                <div class="col-md-4 col-sm-12 col-lg-4">
+                    <div class="card bg-light d-flex flex-fill">
+                        <div class="card-header text-muted border-bottom-0 pb-0 mb-0">
+                            {{ authUserStore.user.jabatan }}
+                        </div>
+                        <div class="card-body pt-0">
+                            <div class="row">
+                                <div class="col-12">
+                                    <h2 class="lead"><b>{{ authUserStore.user.name }}</b></h2>
+                                    <!-- <p class="text-muted text-sm"><b>NIP: </b> {{ authUserStore.user.username }} </p> -->
+                                    <ul class="ml-5 mb-0 fa-ul text-muted">
+                                        <li class="small"><span class="fa-li"><i class="far fa-id-card"></i></span> {{ authUserStore.user.username }}</li>
+                                        <li class="small"><span class="fa-li"><i
+                                                    class="fas fa-lg fa-building"></i></span> {{ authUserStore.user.org_name }}</li>
+                                    </ul>
+                                </div>
+                                <!-- <div class="col-2 text-center">
+                                    <img :src="authUserStore.user.avatar" alt="user-avatar"
+                                        class="img-circle img-fluid">
+                                </div> -->
+                            </div>
+                        </div>
+                        <div class="card-footer b-0 p-0">
+                            <div class="btn-group" style="width: 100% !important;">
+                                <!-- <a href="#" class="btn btn-sm bg-teal">
+                                    <i class="fas fa-comments"></i>
+                                </a> -->
+                                <router-link to="/admin/profile" class="btn btn-sm btn-primary" style="display: block; font-size: small;">
+                                    <i class="fas fa-user"></i>&nbsp;Lihat Profil
+                                </router-link>
+                                <router-link to="/admin/reports/create" class="btn btn-sm btn-success" style="display: block; font-size: small;">
+                                    Input Kegiatan <i class="fas fa-arrow-circle-right"></i>
+                                </router-link>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+                <!-- <div class="col-lg-3 col-12 col-md-4">
                     <div class="small-box bg-info">
                         <div v-if="loadingStore.isLoading" class="overlay">
                             <i class="fas fa-2x fa-sync-alt fa-spin"></i>
@@ -62,7 +100,8 @@ onMounted(() => {
                                 <h3>{{ dashboardStore.totalWorksCount }}</h3>
                                 <select v-model="monthYearStore.mySelected" @change="getSummary()"
                                     style="height: 2rem; outline: 2px solid transparent;" class="px-1 rounded border-0">
-                                    <option v-for="opt in monthYearStore.myOptions" :value="opt.id" :key="opt.id">{{ opt.text }}</option>
+                                    <option v-for="opt in monthYearStore.myOptions" :value="opt.id" :key="opt.id">{{
+                    opt.text }}</option>
                                 </select>
                             </div>
                             <p>Laporan Kerja</p>
@@ -71,11 +110,11 @@ onMounted(() => {
                             <i class="ion ion-bag"></i>
                         </div>
                         <router-link to="/admin/reports/create" class="small-box-footer">
-                            Buat Kinerja
+                            Input Kegiatan
                             <i class="fas fa-arrow-circle-right"></i>
                         </router-link>
                     </div>
-                </div>
+                </div> -->
 
                 <!-- <div class="col-lg-3 col-6">
 
@@ -121,11 +160,13 @@ onMounted(() => {
                                     </p>
 
 
-                                    <div class="progress-group" v-for="stat in dashboardStore.reportCompletion" :key="stat.org_name">
+                                    <div class="progress-group" v-for="stat in dashboardStore.reportCompletion"
+                                        :key="stat.org_name">
                                         {{ stat.org_name }}
                                         <span class="float-right"><b>{{ stat.count }}</b>/ {{ stat.users_count }}</span>
                                         <div class="progress progress-sm">
-                                            <div class="progress-bar bg-primary" :style="{ width: stat.percentage + '%' }">
+                                            <div class="progress-bar bg-primary"
+                                                :style="{ width: stat.percentage + '%' }">
                                             </div>
                                         </div>
                                     </div>
